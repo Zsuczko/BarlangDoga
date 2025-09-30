cf2621c9e0994f873de2143ba84e975d5d286cb3
zsuczko <instabb6464@gmail.com>
2025-09-30 08:08:07 +0200

    Osztály létre hozzása

---
+++ BarlangDoga/BarlangDoga/Models/Barlang.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarlangDoga.Models
+{
+    public class Barlang
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nev { get; set; }
+        public int Hossz { get; set; }
+        public int Kiterjedes { get; set; }
+        public int Melyseg { get; set; }
+        public int Magassag { get; set; }
+        public string Telepules { get; set; }
+    }
+}
